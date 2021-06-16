27dd354ea77a8bb088dd3b9cf2882e49e91bcb3f
305companyofficial <72693450+305companyofficial@users.noreply.github.com>
2021-06-16 17:19:02 +0900

    6.16 first commit second commit NaverMap.kt

--- app/src/main/java/kr/loplab/gnss05/MyDialog.kt
+++ app/src/main/java/kr/loplab/gnss05/MyDialog.kt
@@ -20,7 +20,7 @@ class MyDialog(context : Context)  {
     fun start(content : String) {
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)   //타이틀바 제거
         dialog.setContentView(R.layout.dialog1)     //다이얼로그에 사용할 xml 파일을 불러옴
-        dialog.setCancelable(false)    //다이얼로그의 바깥 화면을 눌렀을 때 다이얼로그가 닫히지 않도록 함
+        dialog.setCancelable(true)    //다이얼로그의 바깥 화면을 눌렀을 때 다이얼로그가 닫히지 않도록 함
 
         // 리사이클러뷰에 표시할 데이터 리스트 생성.
         // 리사이클러뷰에 표시할 데이터 리스트 생성.
