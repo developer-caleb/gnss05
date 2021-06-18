2ac36083c72a20ea84554981cdccca677f6547ed
305companyofficial <72693450+305companyofficial@users.noreply.github.com>
2021-06-18 13:51:39 +0900

    6.18 first commit second commit NaverMap.kt

--- app/src/main/java/kr/loplab/gnss05/FileExportActivity.kt
+++ app/src/main/java/kr/loplab/gnss05/FileExportActivity.kt
@@ -2,11 +2,18 @@ package kr.loplab.gnss05
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class FileExportActivity : AppCompatActivity() {
+    var TAG : String = javaClass.simpleName;
+    lateinit var header : DefaultHeader ;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_file_export)
+        header = findViewById(R.id.header_03)
 
+        header.setOnBackButtonClickListener { v->{
+            Log.d(TAG, "onCreate: ")
+            onBackPressed() } }
     }
 }
