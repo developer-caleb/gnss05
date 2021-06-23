255222e54e09df6ec3474f3a2115974cc254a8b0
305companyofficial <72693450+305companyofficial@users.noreply.github.com>
2021-06-23 15:49:03 +0900

    6.23 시작!!
    일단 되긴 되는 거 같음

---
+++ app/src/main/java/kr/loplab/gnss05/UserFormatView.kt
@@ -0,0 +1,22 @@
+package kr.loplab.gnss05
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kr.loplab.gnss02.ActivityBase
+import kr.loplab.gnss05.databinding.ActivityUserFormatBinding
+import kr.loplab.gnss05.databinding.ActivityUserFormatViewBinding
+
+class UserFormatView : ActivityBase<ActivityUserFormatViewBinding>(){
+    override val layoutResourceId: Int
+        get() = R.layout.activity_user_format_view
+
+
+    override fun init() {
+    }
+
+    override fun initListener() {
+    }
+
+    override fun initDatabinding() {
+    }
+}
