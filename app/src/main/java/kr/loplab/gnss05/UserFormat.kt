1e49e81caaf6d09b5830fbd778b8beadbb677581
305companyofficial <72693450+305companyofficial@users.noreply.github.com>
2021-06-23 13:13:53 +0900

    6.23 시작!!
    일단 되긴 되는 거 같음

--- app/src/main/java/kr/loplab/gnss05/UserFormat.kt
+++ app/src/main/java/kr/loplab/gnss05/UserFormat.kt
@@ -2,10 +2,28 @@ package kr.loplab.gnss05
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kr.loplab.gnss02.ActivityBase
+import kr.loplab.gnss05.databinding.ActivityStandardPointBinding
+import kr.loplab.gnss05.databinding.ActivityUserFormatBinding
 
-class UserFormat : AppCompatActivity() {
+class UserFormat : ActivityBase<ActivityUserFormatBinding>()  {
+    override val layoutResourceId: Int
+        get() = R.layout.activity_user_format
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_user_format)
+
+
+    }
+
+    override fun init() {
+     //
+    }
+
+    override fun initListener() {
+      //
+    }
+
+    override fun initDatabinding() {
+       //
     }
 }
---
+++ app/src/main/java/kr/loplab/gnss05/UserFormatRecyclerViewAdapter.kt
@@ -0,0 +1,77 @@
+package kr.loplab.gnss05
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class UserFormatRecyclerViewAdapter internal constructor(context: Context?, data: ArrayList<Array<String>>) :
+    RecyclerView.Adapter<UserFormatRecyclerViewAdapter.ViewHolder>() {
+    private val mData: ArrayList<Array<String>>
+    private val mInflater: LayoutInflater
+    private var mClickListener: RecyclerItemClickListener? = null
+
+    // inflates the cell layout from xml when needed
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = mInflater.inflate(R.layout.recyclerview_custom_format, parent, false)
+        return ViewHolder(view)
+    }
+
+    // binds the data to the TextView in each cell
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.myTextView.text = mData[position][0]
+        /*when(mData[position][2]){
+           *//* "rightarrow" -> holder.imageView.setImageResource(R.drawable.ic_rightarrow2)
+            "back" -> holder.imageView.setImageResource(R.drawable.lefticon)
+            "folder" -> holder.imageView.setImageResource(R.drawable.foldericon)
+                ".csv" ->  holder.imageView.setImageResource(R.drawable.excelicon)
+            else -> holder.imageView.setImageResource(R.drawable.unknown_file)*//*
+        }*/
+
+    }
+
+    // total number of cells
+    override fun getItemCount(): Int {
+        return mData.size
+    }
+
+    // stores and recycles views as they are scrolled off screen
+    inner class ViewHolder internal constructor(itemView: View) : RecyclerView.ViewHolder(itemView),
+        View.OnClickListener {
+        var myTextView: TextView
+        //var imageView: ImageView
+        override fun onClick(view: View) {
+            if (mClickListener != null) mClickListener!!.onItemClick(view, adapterPosition)
+        }
+
+        init {
+            myTextView = itemView.findViewById(R.id.list_text2)
+            //imageView = itemView.findViewById(R.id.recyclerview_icon)
+            itemView.setOnClickListener(this)
+        }
+    }
+
+    // convenience method for getting data at click position
+    fun getItem(id: Int): String {
+        return mData[id][0]
+    }
+
+    // allows clicks events to be caught
+    fun setClickListener(itemClickListener: RecyclerItemClickListener?) {
+        mClickListener = itemClickListener
+    }
+
+    // parent activity will implement this method to respond to click events
+    interface RecyclerItemClickListener {
+        fun onItemClick(view: View?, position: Int)
+    }
+
+    // data is passed into the constructor
+    init {
+        mInflater = LayoutInflater.from(context)
+        mData = data
+    }
+}
