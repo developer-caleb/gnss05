6573724dd2a7a8382a0f8a3a58ba77c62a805d52
305companyofficial <72693450+305companyofficial@users.noreply.github.com>
2021-07-08 17:01:19 +0900

    7.8 시작!!
    일단 되긴 되는 거 같음

---
+++ app/src/main/java/kr/loplab/gnss05/tablelayout_tools/HorizontalScroll.java
@@ -0,0 +1,38 @@
+package kr.loplab.gnss05.tablelayout_tools;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.HorizontalScrollView;
+
+public class HorizontalScroll extends HorizontalScrollView {
+
+    private ScrollViewListener scrollViewListener = null;
+    public interface ScrollViewListener {
+
+        void onScrollChanged(HorizontalScroll scrollView, int x, int y, int oldx, int oldy);
+
+    }
+    public HorizontalScroll(Context context) {
+        super(context);
+    }
+
+    public HorizontalScroll(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public HorizontalScroll(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public void setScrollViewListener(ScrollViewListener scrollViewListener) {
+        this.scrollViewListener = scrollViewListener;
+    }
+
+    @Override
+    protected void onScrollChanged(int x, int y, int oldx, int oldy) {
+        super.onScrollChanged(x, y, oldx, oldy);
+        if(scrollViewListener != null) {
+            scrollViewListener.onScrollChanged(this, x, y, oldx, oldy);
+        }
+    }
+}
