4080779d633b9f21131a21f00aeaafc697d184c2
305companyofficial <72693450+305companyofficial@users.noreply.github.com>
2021-08-10 16:57:09 +0900

    8.9 table 완료 main page 세팅 중(45%) -vertical layout(완성) //setting activity 제작하여 변경하기
    //adapter 변경해야함 4개로 나눠야 scroll 정보가 다 바뀜
    20%

--- app/src/main/java/kr/loplab/gnss05/MainActivity.kt
+++ app/src/main/java/kr/loplab/gnss05/MainActivity.kt
@@ -25,6 +25,7 @@ import com.karumi.dexter.PermissionToken
 import com.karumi.dexter.listener.PermissionRequest
 import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 import kr.loplab.gnss05.SplashActivity.Gnssreceiver2
+import kr.loplab.gnss05.adapter.AdapterViewpager
 import kr.loplab.gnss05.common.Define.REQUEST_SETTING
 import kr.loplab.gnss05.databinding.ActivityMainBinding
 import kr.loplab.gnss05.model.MainIcons
@@ -35,16 +36,12 @@ import kotlin.concurrent.thread
 
 
 class MainActivity : AppCompatActivity(),
-    MainpageRecyclerViewAdapter.RecyclerItemClickListener , DialogRecyclerviewAdapter.RecyclerItemClickListener {
+     DialogRecyclerviewAdapter.RecyclerItemClickListener {
     val TAG = javaClass.simpleName
     private lateinit var binding : ActivityMainBinding
-    var arrayadapter : Array<MainpageRecyclerViewAdapter?> =  kotlin.arrayOfNulls(4)
-    var selectdata = ArrayList<MainIcons>()
-    var datasarray : Array<ArrayList<MainIcons>?> = kotlin.arrayOfNulls(4)
-    var data0 = ArrayList<MainIcons>()
-    var data1 = ArrayList<MainIcons>()
-    var data2 = ArrayList<MainIcons>()
-    var data3 = ArrayList<MainIcons>()
+    var adapterViewpager: AdapterViewpager? = null
+
+
     var tabposition =0;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -67,70 +64,11 @@ class MainActivity : AppCompatActivity(),
         //setContentView(R.layout.activity_main)
 
 
-        data0.add(MainIcons(R.drawable.ic_0_0_work, "작업"))
-        data0.add(MainIcons(R.drawable.ic_0_1_work_group, "작업그룹"))
-        data0.add(MainIcons(R.drawable.ic_0_2_coordinate, "좌표계"))
-        data0.add(MainIcons(R.drawable.ic_0_3_correction, "점 보정"))
-        data0.add(MainIcons(R.drawable.ic_0_4_pointsave, "점저장소"))
-        data0.add(MainIcons(R.drawable.ic_0_5_export, "내보내기", UserFormatMake::class.java))
-        data0.add(MainIcons(R.drawable.ic_0_6_scan, "스캔"))
-        data0.add(MainIcons(R.drawable.ic_0_7_cloud, "클라우드"))
-        data0.add(MainIcons(R.drawable.ic_0_8_settings, "설정", SettingActivity::class.java))
-        data0.add(MainIcons(R.drawable.ic_0_9_information, "정보"))
 
-        data1.add(MainIcons(R.drawable.ic_1_0_connect_device, "장비연결"))
-        data1.add(MainIcons(R.drawable.ic_1_1_move_country, "이동국"))
-        data1.add(MainIcons(R.drawable.ic_1_2_reference_country, "기준국"))
-        data1.add(MainIcons(R.drawable.ic_1_3_stop_survey, "정지측량"))
-        data1.add(MainIcons(R.drawable.ic_1_4_connection_state, "연결상태"))
-        data1.add(MainIcons(R.drawable.ic_1_5_connection_save, "연결저장"))
-        data1.add(MainIcons(R.drawable.ic_1_6_connection_infomation, "연결정보"))
-        data1.add(MainIcons(R.drawable.ic_1_7_sensor_correction, "센서보정"))
-        data1.add(MainIcons(R.drawable.ic_1_8_setting_equipment, "장비설정"))
-        data1.add(MainIcons(R.drawable.ic_1_9_frequency_setting, "주파수 설정"))
-        data1.add(MainIcons(R.drawable.ic_1_10_change_location, "위치변경"))
-        data1.add(MainIcons(R.drawable.ic_1_11_enroll_equipment, "장비등록"))
-
-        data2.add(MainIcons(R.drawable.ic_2_0_status_work, "현황작업"))
-        data2.add(MainIcons(R.drawable.ic_2_1_detail_survey, "상세 측량"))
-        data2.add(MainIcons(R.drawable.ic_2_2_cad, "CAD (측설)"))
-        data2.add(MainIcons(R.drawable.ic_2_3_point_stack_out, "점측설"))
-        data2.add(MainIcons(R.drawable.ic_2_4_line_stack_out, "라인측설"))
-        data2.add(MainIcons(R.drawable.ic_2_5_gis, "GIS"))
-        data2.add(MainIcons(R.drawable.ic_2_6_road_stack_out, "도로측설"))
-        data2.add(MainIcons(R.drawable.ic_2_7_road_offset, "도로옵셋"))
-        data2.add(MainIcons(R.drawable.ic_2_8_cross_section_measurement, "횡단면 측정"))
-        data2.add(MainIcons(R.drawable.ic_2_9_cross_survey, "횡단측량"))
-        data2.add(MainIcons(R.drawable.ic_2_10_bridge_survey, "교량측량"))
-        data2.add(MainIcons(R.drawable.ic_2_11_conical_superelevation_stakeout, "원추형 편경사측설"))
-        data2.add(MainIcons(R.drawable.ic_2_12_wire_stakeout, "전선측설"))
-        data2.add(MainIcons(R.drawable.ic_2_13_electric_stakeout, "전기 측설"))
-        data2.add(MainIcons(R.drawable.ic_2_14_level_control, "레벨제어"))
-        data2.add(MainIcons(R.drawable.ic_2_15_curve_stakeout, "곡선측설"))
-        data2.add(MainIcons(R.drawable.ic_2_16_exist_measurement, "기존측정"))
-        data2.add(MainIcons(R.drawable.ic_2_17_railroad_stakeout, "철길측설"))
-        data2.add(MainIcons(R.drawable.ic_2_18_layer, "레이어"))
-        data2.add(MainIcons(R.drawable.ic_2_19_setting_measuring_range, "측정 범위 설정"))
-
-        data3.add(MainIcons(R.drawable.ic_3_0_calibration, "캘리브레이션"))
-        data3.add(MainIcons(R.drawable.ic_3_1_coordinate_inverse_calculation, "좌표역계산"))
-        data3.add(MainIcons(R.drawable.ic_3_2_angle_converter, "각도변환기"))
-        data3.add(MainIcons(R.drawable.ic_3_3_area_calculation, "면적계산"))
-        data3.add(MainIcons(R.drawable.ic_3_4_cogo_calculation, "COGO계산"))
-        data3.add(MainIcons(R.drawable.ic_3_5_calculation, "계산기"))
-        data3.add(MainIcons(R.drawable.ic_3_6_external_radio, "외장라디오"))
-        data3.add(MainIcons(R.drawable.ic_3_7_calculation_volumn, "볼륨계산"))
-        data3.add(MainIcons(R.drawable.ic_3_8_reset_location, "위치 재설정"))
-        data3.add(MainIcons(R.drawable.ic_3_9_ftp_share, "FTP 공유"))
-        data3.add(MainIcons(R.drawable.ic_3_10_share, "공유"))
-        datasarray[0] = data0
-        datasarray[1] = data1
-        datasarray[2] = data2
-        datasarray[3] = data3
 
 
         permissionchecking()
-        datasarray[0]?.let { selectdata.addAll(it) };
+
 
         // set up the RecyclerView
 
@@ -140,10 +78,8 @@ class MainActivity : AppCompatActivity(),
 
     fun init(){
        // val gnssreceiver3 = Gnssreceiver3()
-
-
-
-
+        adapterViewpager = AdapterViewpager(this)
+        binding.pager1.adapter = adapterViewpager
     }
 
     fun initListener(){
@@ -209,17 +145,15 @@ class MainActivity : AppCompatActivity(),
             }
 
         })
-
-
     }
 
    fun settingRecyclerView(position :Int){
-       binding.recyclerviewMain.layoutManager = LinearLayoutManager(this);
+     /*  binding.recyclerviewMain.layoutManager = LinearLayoutManager(this);
        arrayadapter[position] = MainpageRecyclerViewAdapter(this, datasarray[position]!!, R.layout.recyclerview_item_vertical)
        //binding.recyclerviewMain.layoutManager = GridLayoutManager(this, 3)
        //adapter = MainpageRecyclerViewAdapter(this, selectdata, R.layout.recyclerview_item_grid)
        arrayadapter[position]?.setClickListener(this)
-       binding.recyclerviewMain.adapter = arrayadapter[position]
+       binding.recyclerviewMain.adapter = arrayadapter[position]*/
 
    }
 
@@ -233,53 +167,6 @@ class MainActivity : AppCompatActivity(),
    }
 
 
-    override fun onItemClick(view: View?, position: Int) {
-        Log.d(TAG, "onItemClick: $position clicked!")
-        when (position){
-      /*      0 ->    { intent = Intent(this, StandardPointActivity::class.java)
-                startActivity(intent);}
-            1 ->    { intent = Intent(this, NaverMap::class.java)
-                startActivity(intent);}
-            2 -> {
-                val dlg = MyDialog(this)
-                dlg.setClickListener(this)
-                dlg.setOnOKClickedListener{ content ->
-                    Log.d(TAG, "onItemClick: $content")
-                }
-                dlg.start("메인의 내용을 변경할까요?")
-            }
-            3 -> {
-                val nextIntent = Intent(this, ColorPickerdialog::class.java)
-                startActivity(nextIntent);
-            }
-            4 -> {
-                val nextIntent = Intent(this, FileExportActivity::class.java)
-                startActivity(nextIntent);
-            }
-            5 -> {
-                val nextIntent = Intent(this, UserFormatMake::class.java)
-                startActivity(nextIntent);
-            }
-
-
-            6 -> {
-                //val nextIntent = Intent(this, TableActivity::class.java)
-                val nextIntent = Intent(this, TableMainActivity::class.java)
-                startActivity(nextIntent);
-            }
-            7 -> {
-                //val nextIntent = Intent(this, TableActivity::class.java)
-                val nextIntent = Intent(this, TableMainActivity::class.java)
-                startActivity(nextIntent);
-            }
-            8 -> {
-                //val nextIntent = Intent(this, TableActivity::class.java)
-                val nextIntent = Intent(this, TableMainActivity::class.java)
-                startActivity(nextIntent);
-
-            }*/
-        }
-    }
 
 
 
---
+++ app/src/main/java/kr/loplab/gnss05/adapter/AdapterViewpager.kt
@@ -0,0 +1,183 @@
+package kr.loplab.gnss05.adapter
+
+import android.content.Context
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.viewpager.widget.PagerAdapter
+import kr.loplab.gnss05.MainpageRecyclerViewAdapter
+import kr.loplab.gnss05.R
+import kr.loplab.gnss05.SettingActivity
+import kr.loplab.gnss05.UserFormatMake
+import kr.loplab.gnss05.databinding.ToolViewpageradapterBinding
+import kr.loplab.gnss05.model.MainIcons
+
+class AdapterViewpager: PagerAdapter, MainpageRecyclerViewAdapter.RecyclerItemClickListener {
+
+    public var arrlist = arrayOf("국립환경인재개발원", "국립생물자원관", "환경산업연구단지", "국립환경과학원")
+    // LayoutInflater 서비스 사용을 위한 Context 참조 저장.
+    private var mContext: Context? = null
+    lateinit var binding : ToolViewpageradapterBinding
+    var selectdata = ArrayList<MainIcons>()
+    var datasarray : Array<ArrayList<MainIcons>?> = kotlin.arrayOfNulls(4)
+    var arrayadapter : Array<MainpageRecyclerViewAdapter?> =  kotlin.arrayOfNulls(4)
+    var data0 = ArrayList<MainIcons>()
+    var data1 = ArrayList<MainIcons>()
+    var data2 = ArrayList<MainIcons>()
+    var data3 = ArrayList<MainIcons>()
+    constructor() {}
+    // Context를 전달받아 mContext에 저장하는 생성자 추가.
+    constructor(context: Context?) {
+        mContext = context
+    }
+    override fun getCount(): Int {
+        return arrlist.size
+    }
+
+    override fun instantiateItem(container: ViewGroup, position: Int): Any {
+        if (mContext != null) {
+            binding = ToolViewpageradapterBinding.inflate(LayoutInflater.from(mContext), container, false)
+
+            initDefault()
+            init(position)
+            initListener(position)
+            initDatabinding(position)
+
+        }
+            binding.textView.text = arrlist[position]
+        container.addView(binding.root)
+        return binding.root
+    }
+    override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
+        // 뷰페이저에서 삭제.
+        container.removeView(`object` as View)
+    }
+
+    override fun isViewFromObject(view: View, `object`: Any): Boolean {
+        return view === `object` as View
+    }
+    fun initDefault(){
+        data0.add(MainIcons(R.drawable.ic_0_0_work, "작업"))
+        data0.add(MainIcons(R.drawable.ic_0_1_work_group, "작업그룹"))
+        data0.add(MainIcons(R.drawable.ic_0_2_coordinate, "좌표계"))
+        data0.add(MainIcons(R.drawable.ic_0_3_correction, "점 보정"))
+        data0.add(MainIcons(R.drawable.ic_0_4_pointsave, "점저장소"))
+        data0.add(MainIcons(R.drawable.ic_0_5_export, "내보내기", UserFormatMake::class.java))
+        data0.add(MainIcons(R.drawable.ic_0_6_scan, "스캔"))
+        data0.add(MainIcons(R.drawable.ic_0_7_cloud, "클라우드"))
+        data0.add(MainIcons(R.drawable.ic_0_8_settings, "설정", SettingActivity::class.java))
+        data0.add(MainIcons(R.drawable.ic_0_9_information, "정보"))
+
+        data1.add(MainIcons(R.drawable.ic_1_0_connect_device, "장비연결"))
+        data1.add(MainIcons(R.drawable.ic_1_1_move_country, "이동국"))
+        data1.add(MainIcons(R.drawable.ic_1_2_reference_country, "기준국"))
+        data1.add(MainIcons(R.drawable.ic_1_3_stop_survey, "정지측량"))
+        data1.add(MainIcons(R.drawable.ic_1_4_connection_state, "연결상태"))
+        data1.add(MainIcons(R.drawable.ic_1_5_connection_save, "연결저장"))
+        data1.add(MainIcons(R.drawable.ic_1_6_connection_infomation, "연결정보"))
+        data1.add(MainIcons(R.drawable.ic_1_7_sensor_correction, "센서보정"))
+        data1.add(MainIcons(R.drawable.ic_1_8_setting_equipment, "장비설정"))
+        data1.add(MainIcons(R.drawable.ic_1_9_frequency_setting, "주파수 설정"))
+        data1.add(MainIcons(R.drawable.ic_1_10_change_location, "위치변경"))
+        data1.add(MainIcons(R.drawable.ic_1_11_enroll_equipment, "장비등록"))
+
+        data2.add(MainIcons(R.drawable.ic_2_0_status_work, "현황작업"))
+        data2.add(MainIcons(R.drawable.ic_2_1_detail_survey, "상세 측량"))
+        data2.add(MainIcons(R.drawable.ic_2_2_cad, "CAD (측설)"))
+        data2.add(MainIcons(R.drawable.ic_2_3_point_stack_out, "점측설"))
+        data2.add(MainIcons(R.drawable.ic_2_4_line_stack_out, "라인측설"))
+        data2.add(MainIcons(R.drawable.ic_2_5_gis, "GIS"))
+        data2.add(MainIcons(R.drawable.ic_2_6_road_stack_out, "도로측설"))
+        data2.add(MainIcons(R.drawable.ic_2_7_road_offset, "도로옵셋"))
+        data2.add(MainIcons(R.drawable.ic_2_8_cross_section_measurement, "횡단면 측정"))
+        data2.add(MainIcons(R.drawable.ic_2_9_cross_survey, "횡단측량"))
+        data2.add(MainIcons(R.drawable.ic_2_10_bridge_survey, "교량측량"))
+        data2.add(MainIcons(R.drawable.ic_2_11_conical_superelevation_stakeout, "원추형 편경사측설"))
+        data2.add(MainIcons(R.drawable.ic_2_12_wire_stakeout, "전선측설"))
+        data2.add(MainIcons(R.drawable.ic_2_13_electric_stakeout, "전기 측설"))
+        data2.add(MainIcons(R.drawable.ic_2_14_level_control, "레벨제어"))
+        data2.add(MainIcons(R.drawable.ic_2_15_curve_stakeout, "곡선측설"))
+        data2.add(MainIcons(R.drawable.ic_2_16_exist_measurement, "기존측정"))
+        data2.add(MainIcons(R.drawable.ic_2_17_railroad_stakeout, "철길측설"))
+        data2.add(MainIcons(R.drawable.ic_2_18_layer, "레이어"))
+        data2.add(MainIcons(R.drawable.ic_2_19_setting_measuring_range, "측정 범위 설정"))
+
+        data3.add(MainIcons(R.drawable.ic_3_0_calibration, "캘리브레이션"))
+        data3.add(MainIcons(R.drawable.ic_3_1_coordinate_inverse_calculation, "좌표역계산"))
+        data3.add(MainIcons(R.drawable.ic_3_2_angle_converter, "각도변환기"))
+        data3.add(MainIcons(R.drawable.ic_3_3_area_calculation, "면적계산"))
+        data3.add(MainIcons(R.drawable.ic_3_4_cogo_calculation, "COGO계산"))
+        data3.add(MainIcons(R.drawable.ic_3_5_calculation, "계산기"))
+        data3.add(MainIcons(R.drawable.ic_3_6_external_radio, "외장라디오"))
+        data3.add(MainIcons(R.drawable.ic_3_7_calculation_volumn, "볼륨계산"))
+        data3.add(MainIcons(R.drawable.ic_3_8_reset_location, "위치 재설정"))
+        data3.add(MainIcons(R.drawable.ic_3_9_ftp_share, "FTP 공유"))
+        data3.add(MainIcons(R.drawable.ic_3_10_share, "공유"))
+
+        datasarray[0]?.let { selectdata.addAll(it) };
+        datasarray[0] = data0
+        datasarray[1] = data1
+        datasarray[2] = data2
+        datasarray[3] = data3
+    }
+    fun init(position: Int){
+        binding.recyclerviewMain.layoutManager = LinearLayoutManager(mContext);
+        arrayadapter[position] = MainpageRecyclerViewAdapter(mContext, datasarray[position]!!, R.layout.recyclerview_item_vertical)
+        //binding.recyclerviewMain.layoutManager = GridLayoutManager(this, 3)
+        //adapter = MainpageRecyclerViewAdapter(this, selectdata, R.layout.recyclerview_item_grid)
+        arrayadapter[position]?.setClickListener(this)
+        binding.recyclerviewMain.adapter = arrayadapter[position]
+    }
+    fun initListener(position: Int){}
+    fun initDatabinding(position: Int){}
+    override fun onItemClick(view: View?, position: Int) {
+        Log.d("TAG", "onItemClick: $position clicked!")
+        when (position){
+            /*      0 ->    { intent = Intent(this, StandardPointActivity::class.java)
+                      startActivity(intent);}
+                  1 ->    { intent = Intent(this, NaverMap::class.java)
+                      startActivity(intent);}
+                  2 -> {
+                      val dlg = MyDialog(this)
+                      dlg.setClickListener(this)
+                      dlg.setOnOKClickedListener{ content ->
+                          Log.d(TAG, "onItemClick: $content")
+                      }
+                      dlg.start("메인의 내용을 변경할까요?")
+                  }
+                  3 -> {
+                      val nextIntent = Intent(this, ColorPickerdialog::class.java)
+                      startActivity(nextIntent);
+                  }
+                  4 -> {
+                      val nextIntent = Intent(this, FileExportActivity::class.java)
+                      startActivity(nextIntent);
+                  }
+                  5 -> {
+                      val nextIntent = Intent(this, UserFormatMake::class.java)
+                      startActivity(nextIntent);
+                  }
+
+
+                  6 -> {
+                      //val nextIntent = Intent(this, TableActivity::class.java)
+                      val nextIntent = Intent(this, TableMainActivity::class.java)
+                      startActivity(nextIntent);
+                  }
+                  7 -> {
+                      //val nextIntent = Intent(this, TableActivity::class.java)
+                      val nextIntent = Intent(this, TableMainActivity::class.java)
+                      startActivity(nextIntent);
+                  }
+                  8 -> {
+                      //val nextIntent = Intent(this, TableActivity::class.java)
+                      val nextIntent = Intent(this, TableMainActivity::class.java)
+                      startActivity(nextIntent);
+
+                  }*/
+        }
+    }
+
+}
