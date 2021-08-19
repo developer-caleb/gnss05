c8bb90f045a80c52dac55ba812eb694e2b672586
305companyofficial <72693450+305companyofficial@users.noreply.github.com>
2021-08-19 15:12:32 +0900

    8.19 Workmanager 작업 중 Room DB 완료 이제부터 Graph작업 들어감
    1차 수정 괜찮은 듯?

--- app/src/main/java/kr/loplab/gnss05/activities/workmanager/WorkManagerActivity.kt
+++ app/src/main/java/kr/loplab/gnss05/activities/workmanager/WorkManagerActivity.kt
@@ -3,22 +3,54 @@ package kr.loplab.gnss05.activities.workmanager
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextUtils
+import android.text.TextWatcher
+import android.util.Log
+import android.view.View
+import android.widget.*
 import androidx.lifecycle.lifecycleScope
 import androidx.room.Room
+import com.evrencoskun.tableview.TableView
+import com.evrencoskun.tableview.filter.Filter
+import com.evrencoskun.tableview.pagination.Pagination
+import com.evrencoskun.tableview.pagination.Pagination.OnTableViewPageTurnedListener
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kr.loplab.gnss02.ActivityBase
+import kr.loplab.gnss05.DefaultHeader
+import kr.loplab.gnss05.GlobalApplication
 import kr.loplab.gnss05.R
+import kr.loplab.gnss05.activities.UserFormatMake
 import kr.loplab.gnss05.common.Define.WORKERS_DB
 import kr.loplab.gnss05.databinding.ActivityStopSurveyBinding
 import kr.loplab.gnss05.databinding.ActivityWorkManagerBinding
 import kr.loplab.gnss05.tableview.TableMainActivity
+import kr.loplab.gnss05.tableview.TableViewAdapter
+import kr.loplab.gnss05.tableview.TableViewListener
+import kr.loplab.gnss05.tableview.TableViewModel
 
 class WorkManagerActivity : ActivityBase<ActivityWorkManagerBinding>() {
     override val layoutResourceId: Int
         get() = R.layout.activity_work_manager
     lateinit var db : AppDatabase
+
+
+    private var moodFilter: Spinner? = null
+    private  var genderFilter:android.widget.Spinner? = null
+    private var previousButton: ImageButton? = null
+    private  var nextButton:android.widget.ImageButton? = null
+    private var tablePaginationDetails: TextView? = null
+    private lateinit var mTableView: TableView
+    lateinit var tableViewModel: TableViewModel
+    private var mTableFilter // This is used for filtering the table.
+            : Filter? = null
+    private var mPagination // This is used for paginating the table.
+            : Pagination? = null
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -45,6 +77,38 @@ class WorkManagerActivity : ActivityBase<ActivityWorkManagerBinding>() {
 
     override fun initDatabinding() {
         lifecycleScope.launch(Dispatchers.IO) {
-        viewBinding.dbText.text = db.workerDao().all.toString()}
+            viewBinding.dbText.text = db.workerDao().all.toString()
+        }
+        // Let's get TableView
+        mTableView = findViewById(R.id.tableview2)
+
+        initializeTableView()
     }
+
+     fun initializeTableView() {
+        // Create TableView View model class  to group view models of TableView
+        tableViewModel = TableViewModel()
+
+        // Create TableView Adapter
+        val tableViewAdapter = TableViewAdapter(tableViewModel)
+        mTableView.setAdapter(tableViewAdapter)
+        mTableView.setTableViewListener(TableViewListener(mTableView))
+
+        // Create an instance of a Filter and pass the TableView.
+        //mTableFilter = new Filter(mTableView);
+
+        // Load the dummy data to the TableView
+        tableViewAdapter.setAllItems(
+            tableViewModel.getColumnHeaderList(), tableViewModel
+                .getRowHeaderList(), tableViewModel.getCellList()
+        )
+
+    }
+
+
+
+
+
+
+
 }
