163c7652a6757d100d6cba1c89c2626bb231bfa8
305companyofficial <72693450+305companyofficial@users.noreply.github.com>
2021-08-24 11:03:54 +0900

    8.23 변경완료, Size 기기별 해상도 맞춤

--- app/src/main/java/kr/loplab/gnss05/activities/SettingSatelliteActivity.kt
+++ app/src/main/java/kr/loplab/gnss05/activities/SettingSatelliteActivity.kt
@@ -3,8 +3,13 @@ package kr.loplab.gnss05.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Switch
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_setting_satellite.*
 import kr.loplab.gnss02.ActivityBase
 import kr.loplab.gnss05.R
+import kr.loplab.gnss05.common.Define.*
+import kr.loplab.gnss05.common.PrefUtil
 import kr.loplab.gnss05.databinding.ActivitySettingSatelliteBinding
 import kr.loplab.gnss05.databinding.ActivityStopSurveyBinding
 
@@ -13,7 +18,6 @@ class SettingSatelliteActivity : ActivityBase<ActivitySettingSatelliteBinding>()
         get() = R.layout.activity_setting_satellite
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_setting_satellite)
     }
 
     override fun init() {
@@ -23,12 +27,60 @@ class SettingSatelliteActivity : ActivityBase<ActivitySettingSatelliteBinding>()
     override fun initListener() {
         viewBinding.header01.setOnBackButtonClickListener { onBackPressed();}
         viewBinding.btConfirm.setOnClickListener {
-            Log.d(TAG, "initListener: confirmbt_clicked")
+            Log.d(TAG, "initListener: confirmbt_clicked-> 저장하기")
+            //save
+            onBackPressed()
+        }
+        viewBinding.layoutGps.setOnClickListener {
+            Log.d(TAG, "initListener: clicked")
+            swchange(viewBinding.swGps)
+        }
+        viewBinding.layoutGlonass.setOnClickListener {
+            Log.d(TAG, "initListener: clicked")
+            swchange(viewBinding.swGlonass)
+        }
+        viewBinding.layoutBeidou.setOnClickListener {
+            Log.d(TAG, "initListener: clicked")
+            viewBinding.swBeidou.isChecked =    !viewBinding.swBeidou.isChecked
+        }
+        viewBinding.layoutGalilieo.setOnClickListener {
+            Log.d(TAG, "initListener: clicked")
+            viewBinding.swGalieo.isChecked =    !viewBinding.swGalieo.isChecked
+        }
+        viewBinding.layoutSbas.setOnClickListener {
+            Log.d(TAG, "initListener: clicked")
+            swchange2(viewBinding.swSbas)
+        }
+        viewBinding.layoutQzss.setOnClickListener {
+            Log.d(TAG, "initListener: clicked")
+            swchange2(viewBinding.swQzss)
+        }
+        viewBinding.layoutLband.setOnClickListener {
+            Log.d(TAG, "initListener: clicked")
+            swchange2(viewBinding.swLband)
         }
 
+        
+
+    }
+    fun swchange(sw : Switch){
+        sw.isChecked = !sw.isChecked
+        //sw.performClick()
+    }
+    fun swchange2(sw : Switch){
+        // sw.isChecked = !sw.isChecked
+        sw.performClick()
     }
 
     override fun initDatabinding() {
+        viewBinding.swGps.isChecked = PrefUtil.getBoolean(this, REFERENCE_COUNTRY_SATELLITE_GPS)
+        viewBinding.swGlonass.isChecked = PrefUtil.getBoolean(this, REFERENCE_COUNTRY_SATELLITE_GLONASS)
+        viewBinding.swBeidou.isChecked = PrefUtil.getBoolean(this, REFERENCE_COUNTRY_SATELLITE_BEIDOU)
+        viewBinding.swGalieo.isChecked = PrefUtil.getBoolean(this, REFERENCE_COUNTRY_SATELLITE_GALILEO)
+        viewBinding.swSbas.isChecked = PrefUtil.getBoolean(this, REFERENCE_COUNTRY_SATELLITE_SBAS)
+        viewBinding.swQzss.isChecked = PrefUtil.getBoolean(this, REFERENCE_COUNTRY_SATELLITE_QZSS)
+        viewBinding.swLband.isChecked = PrefUtil.getBoolean(this, REFERENCE_COUNTRY_SATELLITE_LBAND)
+
 
     }
 }
