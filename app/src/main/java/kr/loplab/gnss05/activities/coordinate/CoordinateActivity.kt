e91e4a0b772bbc4bd66670ce05f83aff044cc868
305companyofficial <72693450+305companyofficial@users.noreply.github.com>
2021-08-31 08:56:52 +0900

    8.31 좌표계 매개 변수 시작 정지측량 시작(완료) 위성설정(완료)

--- app/src/main/java/kr/loplab/gnss05/activities/coordinate/CoordinateActivity.kt
+++ app/src/main/java/kr/loplab/gnss05/activities/coordinate/CoordinateActivity.kt
@@ -1,19 +1,25 @@
-package kr.loplab.gnss05.activities
+package kr.loplab.gnss05.activities.coordinate
 
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import kr.loplab.gnss02.ActivityBase
 import kr.loplab.gnss05.R
+import kr.loplab.gnss05.activities.viewmodel.CoordinateViewModel
+import kr.loplab.gnss05.activities.viewmodel.ReferenceCountryViewModel
 import kr.loplab.gnss05.databinding.ActivityCoordinateBinding
 
 class CoordinateActivity : ActivityBase<ActivityCoordinateBinding>() {
     override val layoutResourceId: Int
         get() = R.layout.activity_coordinate
+    lateinit var viewModel1:CoordinateViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
 
     override fun init() {
-
+        viewModel1 = ViewModelProvider(this).get(CoordinateViewModel::class.java)
+        viewBinding.viewModel = viewModel1
     }
 
     override fun initListener() {
--- app/src/main/java/kr/loplab/gnss05/adapter/AdapterViewpager.kt
+++ app/src/main/java/kr/loplab/gnss05/adapter/AdapterViewpager.kt
@@ -10,6 +10,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.viewpager.widget.PagerAdapter
 import kr.loplab.gnss05.*
 import kr.loplab.gnss05.activities.*
+import kr.loplab.gnss05.activities.coordinate.CoordinateActivity
 import kr.loplab.gnss05.activities.mobile_station.MobileStationActivity
 import kr.loplab.gnss05.activities.reference_country.ReferenceCountryActivity
 import kr.loplab.gnss05.activities.stop_survey.StopSurveyActivity
