7852af2bd36f624be266c897d59417e77ac1c271
305companyofficial <72693450+305companyofficial@users.noreply.github.com>
2021-09-01 17:10:17 +0900

    9.1 사용자 정의 포맷-> Room 구현하기

--- app/src/main/java/kr/loplab/gnss05/activities/export/CustomFileFormatActivity.kt
+++ app/src/main/java/kr/loplab/gnss05/activities/export/CustomFileFormatActivity.kt
@@ -1,24 +1,28 @@
 package kr.loplab.gnss05.activities.export
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.core.app.ActivityCompat
 import androidx.room.Room
 import com.evrencoskun.tableview.TableView
 import kr.loplab.gnss02.ActivityBase
 import kr.loplab.gnss05.R
 import kr.loplab.gnss05.activities.workmanager.AppDatabase
-import kr.loplab.gnss05.activities.workmanager.WorkerActivity
+import kr.loplab.gnss05.activities.workmanager.Worker
 import kr.loplab.gnss05.common.Define
 import kr.loplab.gnss05.databinding.ActivityCustomFileFormatBinding
-import kr.loplab.gnss05.databinding.ActivityWorkManagerBinding
+import kr.loplab.gnss05.tableview.TableViewAdapter
+import kr.loplab.gnss05.tableview.TableViewListener
 import kr.loplab.gnss05.tableview.TableViewModel
 
 class CustomFileFormatActivity : ActivityBase<ActivityCustomFileFormatBinding>() {
     override val layoutResourceId: Int
         get() = R.layout.activity_custom_file_format
     private lateinit var mTableView: TableView
+    lateinit var tableFileFormatViewModel: TableViewModel
+    lateinit var tableViewAdapter : TableViewAdapter
+
     lateinit var db : AppDatabase
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,11 +43,53 @@ class CustomFileFormatActivity : ActivityBase<ActivityCustomFileFormatBinding>()
         viewBinding.header01.setOnBackButtonClickListener { onBackPressed();}
         viewBinding.btAdd.setOnClickListener {
             intent = Intent(this, UserFormatMake::class.java)
-            ActivityCompat.startActivityForResult(this, intent, Define.REQUEST_WORKER_MANAGE_ADD, null)
+            ActivityCompat.startActivityForResult(this, intent, Define.REQUEST_FILE_FORMAT_ADD, null)
+        }
+        viewBinding.btEdit.setOnClickListener {
+            if(TableViewModel.selectedIndex<=-1 || TableViewModel.selectedIndex>=tableFileFormatViewModel.rowHeaderList.size){
+                showToast("수정할 행을 선택해주세요."); return@setOnClickListener
+            }
+            Log.d(TAG, "initListener: edit ${TableViewModel.selectedIndex}")
+            intent = Intent(this, UserFormatMake::class.java)
+            intent.putExtra("selectPosition", TableViewModel.selectedIndex);
+            ActivityCompat.startActivityForResult(this, intent, Define.REQUEST_FILE_FORMAT_EDIT, null)
+        }
+        viewBinding.btDelete.setOnClickListener {
+            // mTableView.
+            if(TableViewModel.selectedIndex<=-1 || TableViewModel.selectedIndex>=tableFileFormatViewModel.rowHeaderList.size){
+                showToast("삭제할 행을 선택해주세요."); return@setOnClickListener
+            }
+            Log.d(TAG, "initListener: delete ${TableViewModel.selectedIndex}")
+            db.fileFormatDao().delete(db.fileFormatDao().all[TableViewModel.selectedIndex])
+            refresh()
+            if(tableFileFormatViewModel.rowHeaderList.size>0){
+                mTableView.selectedRow = 0
+                TableViewModel.selectedIndex = 0}else{
+                mTableView.selectedRow = -1
+                TableViewModel.selectedIndex = -1
+                //mTableView.set
+            }
         }
     }
 
     override fun initDatabinding() {
+        refresh()
+    }
+    fun refresh(){
+        var  fileformatslist : List<FileFormat> = db.fileFormatDao().all
+        mTableView = findViewById(R.id.tableview2)
+        tableFileFormatViewModel = TableFileFormatViewModel(fileformatslist)
+        //추가 시작
+
+        tableViewAdapter = TableViewAdapter(tableFileFormatViewModel)
+        mTableView.setTableViewListener(TableViewListener(mTableView))
+        mTableView.setAdapter(tableViewAdapter)
+        //추가 끝
+        tableViewAdapter.setAllItems(
+            tableFileFormatViewModel.getColumnHeaderList(), tableFileFormatViewModel
+                .getRowHeaderList(), tableFileFormatViewModel.getCellList()
+        )
 
     }
+
 }
---
+++ app/src/main/java/kr/loplab/gnss05/activities/export/TableFileFormatViewModel.java
@@ -0,0 +1,137 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2021 Evren Coşkun
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package kr.loplab.gnss05.activities.export;
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import kr.loplab.gnss05.activities.workmanager.Worker;
+import kr.loplab.gnss05.tableview.TableViewModel;
+import kr.loplab.gnss05.tableview.model.Cell;
+import kr.loplab.gnss05.tableview.model.ColumnHeader;
+import kr.loplab.gnss05.tableview.model.RowHeader;
+
+public class TableFileFormatViewModel extends TableViewModel {
+    String TAG = TableFileFormatViewModel.class.getSimpleName();
+    // Constant size for dummy data sets
+    private static int COLUMN_SIZE = 4;
+    private static int ROW_SIZE = 5;
+    private ArrayList<String> customFileFormat = new ArrayList<>(Arrays.asList("번호", "작업자", "이름", "사용자","암호"));
+    List<FileFormat> fileformatslist =null;
+
+     TableFileFormatViewModel(){}
+     TableFileFormatViewModel(List<FileFormat> fileformatslist){
+         ROW_SIZE = fileformatslist.size();
+        this.fileformatslist = fileformatslist;
+    }
+
+    @Override
+    public void removePosition(int position){
+        Log.d(TAG, "removePosition: override.. 삭제");
+        if(position>= fileformatslist.size()||position<0) return;
+        if(fileformatslist !=null && fileformatslist.size()>0){
+            fileformatslist.remove(position);
+            ROW_SIZE = fileformatslist.size();
+        }else{return;}
+    }
+
+
+    @NonNull
+    private List<RowHeader> getSimpleRowHeaderList() {
+        List<RowHeader> list = new ArrayList<>();
+        for (int i = 0; i < ROW_SIZE; i++) {
+            RowHeader header = new RowHeader(String.valueOf(i), //"row " +
+                   String.valueOf(i+1)
+            );
+            list.add(header);
+        }
+
+        return list;
+    }
+
+    /**
+     * This is a dummy model list test some cases.
+     */
+    @NonNull
+    private List<ColumnHeader> getRandomColumnHeaderList() {
+        List<ColumnHeader> list = new ArrayList<>();
+        for (int i = 0; i < COLUMN_SIZE; i++) {
+            String title = customFileFormat.get(i + 1);
+            ColumnHeader header = new ColumnHeader(String.valueOf(i), title);
+            list.add(header);
+        }
+        return list;
+    }
+
+    /**
+     * This is a dummy model list test some cases.
+     */
+    @NonNull
+    private List<List<Cell>> getCellListForSortingTest() {
+        List<List<Cell>> list = new ArrayList<>();
+        for (int i = 0; i < ROW_SIZE; i++) {
+            List<Cell> cellList = new ArrayList<>();
+            for (int j = 0; j < COLUMN_SIZE; j++) {
+                Object text = "";//"cell " + j + " " + i;
+                //final int random = new Random().nextInt();
+                if (j == 0) {
+                    if(fileformatslist !=null && fileformatslist.size()!=0){
+                        text= fileformatslist.get(i).getFormatName();
+                    }else text= "";
+                } else if (j == 1) {
+                    if(fileformatslist !=null && fileformatslist.size()!=0){
+                        text= fileformatslist.get(i).getExtensionName();
+                    }else text= "";
+                } else if (j == 2) {
+                    if(fileformatslist !=null && fileformatslist.size()!=0){
+                        text= fileformatslist.get(i).getFormatDescription();
+                    }else text= "";
+                }
+                // Create dummy id.
+                String id = j + "-" + i;
+                Cell cell;
+                cell = new Cell(id, text);
+                cellList.add(cell);
+            }
+            list.add(cellList);
+        }
+        return list;
+    }
+
+
+    @NonNull
+    public List<List<Cell>> getCellList() { return getCellListForSortingTest(); }
+
+    @NonNull
+    public List<RowHeader> getRowHeaderList() { return getSimpleRowHeaderList(); }
+
+    @NonNull
+    public List<ColumnHeader> getColumnHeaderList() { return getRandomColumnHeaderList(); }
+}
