83b61ebcfa8f06985e84229ce2d5011463f711f3
305companyofficial <72693450+305companyofficial@users.noreply.github.com>
2021-09-01 14:02:27 +0900

    9.1 //현황작업

--- app/src/main/java/kr/loplab/gnss05/activities/StatusWorkActivity.kt
+++ app/src/main/java/kr/loplab/gnss05/activities/StatusWorkActivity.kt
@@ -19,6 +19,9 @@ class StatusWorkActivity : ActivityBase<ActivityStatusWorkBinding>() {
     }
 
     override fun initListener() {
+        viewBinding.btBack.setOnClickListener {
+            onBackPressed()
+        }
     }
 
     override fun initDatabinding() {
