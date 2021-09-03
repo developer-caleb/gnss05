115d634576a652b75be246d1476c6926792a601f
305companyofficial <72693450+305companyofficial@users.noreply.github.com>
2021-09-03 15:09:28 +0900

    9.3 측량작업 페이지 -> 뷰 제작 중

--- app/src/main/java/kr/loplab/gnss05/activities/measurement/TopoMeasurementActivity.kt
+++ app/src/main/java/kr/loplab/gnss05/activities/measurement/TopoMeasurementActivity.kt
@@ -2,11 +2,27 @@ package kr.loplab.gnss05.activities.measurement
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kr.loplab.gnss02.ActivityBase
 import kr.loplab.gnss05.R
+import kr.loplab.gnss05.databinding.ActivityStatusWorkBinding
+import kr.loplab.gnss05.databinding.ActivityTopoMeasurementBinding
 
-class TopoMeasurementActivity : AppCompatActivity() {
+class TopoMeasurementActivity : ActivityBase<ActivityTopoMeasurementBinding>() {
+    override val layoutResourceId: Int
+        get() = R.layout.activity_topo_measurement
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_topo_measurement)
+    }
+
+    override fun init() {
+
+    }
+
+    override fun initListener() {
+
+    }
+
+    override fun initDatabinding() {
+
     }
 }
