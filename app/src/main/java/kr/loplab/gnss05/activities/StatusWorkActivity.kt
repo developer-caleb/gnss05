2e8baf3fbccd48ae88ce544a243de3b47e60a42d
305companyofficial <72693450+305companyofficial@users.noreply.github.com>
2021-09-03 10:26:47 +0900

    9.2 사용자 정의 포맷-> Room 구현하기 표와 연결하기 Edit 필요(완료)

--- app/src/main/java/kr/loplab/gnss05/activities/StatusWorkActivity.kt
+++ app/src/main/java/kr/loplab/gnss05/activities/StatusWorkActivity.kt
@@ -1,10 +1,14 @@
 package kr.loplab.gnss05.activities
 
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AlertDialog
+import kotlinx.android.synthetic.main.activity_main.*
 import kr.loplab.gnss02.ActivityBase
+import kr.loplab.gnss05.PositionInformationActivity
 import kr.loplab.gnss05.R
 import kr.loplab.gnss05.activities.mobile_station.MobileStationActivity
 import kr.loplab.gnss05.activities.mobile_station.MobileStationSettingSatelliteActivity
@@ -31,6 +35,22 @@ class StatusWorkActivity : ActivityBase<ActivityStatusWorkBinding>() {
             Log.d(TAG, "initListener: settingSatelliteBt clicked")
             intent = Intent(this, MobileStationActivity::class.java)
             startActivity(intent);
+        }
+        viewBinding.btPositionInformation.setOnClickListener {
+            Log.d(TAG, "initListener: settingSatelliteBt clicked")
+            intent = Intent(this, PositionInformationActivity::class.java)
+            startActivity(intent);
+        }
+        viewBinding.btBatteryStatus.setOnClickListener {
+           val alertDialog = AlertDialog.Builder(this)
+               .setTitle("배터리 전원")
+               .setMessage("A현재 배터리 용량:90%\nB현재 배터리 용량:80%")
+               .setPositiveButton("확인"){dialog, which -> {
+                   //클릭하면 뭐할지..
+               }}
+               .create()
+            alertDialog.show()
+            alertDialog.getButton(AlertDialog.BUTTON_POSITIVE).setTextColor(Color.BLUE)
         }
     }
 
