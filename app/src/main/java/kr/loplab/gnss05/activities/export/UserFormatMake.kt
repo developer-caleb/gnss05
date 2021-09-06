b3a74b4414114bbc19d0113624ba35045067d5e5
305companyofficial <72693450+305companyofficial@users.noreply.github.com>
2021-09-06 10:20:33 +0900

    9.6 내보내기

--- app/src/main/java/kr/loplab/gnss05/activities/export/UserFormatMake.kt
+++ app/src/main/java/kr/loplab/gnss05/activities/export/UserFormatMake.kt
@@ -15,6 +15,7 @@ import kr.loplab.gnss05.activities.workmanager.AppDatabase
 import kr.loplab.gnss05.adapter.UserFormatAddRecyclerViewAdapter
 import kr.loplab.gnss05.adapter.UserFormatDeleteRecyclerViewAdapter
 import kr.loplab.gnss05.common.Define
+import kr.loplab.gnss05.common.OptionList
 import kr.loplab.gnss05.common.OptionList.Companion.SEPERATOR_LIST
 import kr.loplab.gnss05.common.PrefUtil
 import kr.loplab.gnss05.databinding.ActivityUserFormatBinding
@@ -29,16 +30,7 @@ class UserFormatMake : ActivityBase<ActivityUserFormatBinding>(),
     UserFormatDeleteRecyclerViewAdapter.RecyclerItemClickListener {
     override val layoutResourceId: Int
         get() = R.layout.activity_user_format
-    var optionitemlist  = arrayOf("이름", "코드", "위도", "경도", "고도", "X", "Y", "Z(레벨)", "X(공간)", "Y(공간)", "Z(공간)", "도로명", "측설점",
-            "단면스테이션", "중간 말뚝거리", "수직거리", "스테이션", "옵셋", "레벨편차", "시작거리", "타워명", "타워번호",
-            "속성유형", "현재날짜", "현재시간", "UTC날짜", "UTC시간", "위성연산", "위성추적중", "수평오차", "수직오차",
-            "Σn", "σe", "PDOP", "HDOP", "VDOP", "저장모드", "솔루션 정보", "Detailed Solution Status", "지연",
-            "컷 오프 각도", "반복횟수", "안테나 높이", "속도", "방향 기준국 ID", "기준국 위도", "기준국 경도",
-            "기준국 고도", "기준국과 거리", "원점 위도", "원점 경도", "원점 고도", "Station Correction x",
-            "Station Correction y", "Station Correction h", "경사 보정", "Pitch", "Roll", "Yaw", "경사 각도", "투영 각도",
-            "X보정", "Y보정", "Z보정", "공분산 Cxx", "공분산 Cxy", "공분산 Cxz", "공분산 Cyx", "공분산 Cyy", "공분산 Cyz",
-            "공분산 Czx", "공분산 Czy", "공분산 Czz", "SD", "HD", "VD", "HA", "VA", "PPM", "참조각도", "알려진 방위각",
-            "스테이션 좌표 북쪽", "스테이션 좌표 동쪽", "스테이션 좌표 높이", "반사경", "HI", "프리즘상수", "HT")
+
     private lateinit var adapterAdd : UserFormatAddRecyclerViewAdapter
     private lateinit var adapterDelete : UserFormatDeleteRecyclerViewAdapter
     var itemdata = ArrayList<Array<String>>()
@@ -55,7 +47,7 @@ class UserFormatMake : ActivityBase<ActivityUserFormatBinding>(),
     //ArrayList(listOf(listOf("/", "1"), listOf("@", "0"), listOf("Space", "0"))) //ArrayList<String>();
     override fun init() {
 
-        optionitemlist.forEachIndexed { index, item -> itemdata.add(arrayOf(item, index.toString(), true.toString()))}
+        OptionList.optionitemlist.forEachIndexed { index, item -> itemdata.add(arrayOf(item, index.toString(), true.toString()))}
         adapterAdd  = UserFormatAddRecyclerViewAdapter(this, itemdata)
         adapterDelete  = UserFormatDeleteRecyclerViewAdapter(this, listdata)
         adapterAdd.setClickListener(this)
--- app/src/main/java/kr/loplab/gnss05/common/OptionList.kt
+++ app/src/main/java/kr/loplab/gnss05/common/OptionList.kt
@@ -47,5 +47,20 @@ class OptionList {
       val DEGREE_FORM_TYPE = arrayListOf<String>("dd.mmsss", "dd:mm:ss.ssss", "ddºmm'ss.ssss\"", "dd (Decimal)", "Radian")
       val DEGREE_FILE_FORM = arrayListOf<String>("측량 점 데이터 형식(.csv)", "TPS 데이터형식(.csv)", "DOL 파일 형식(.csv)",
           "측점보고서(Brief)(.xls)", "측점보고서(Complete)(.xls)", "Gnss measuremnet report (.xls)", "전체 제어점 보고서 (.xls)")
+
+
+      //UserFormatMake
+      var optionitemlist  = arrayOf("이름", "코드", "위도", "경도", "고도", "X", "Y", "Z(레벨)", "X(공간)", "Y(공간)", "Z(공간)", "도로명", "측설점",
+          "단면스테이션", "중간 말뚝거리", "수직거리", "스테이션", "옵셋", "레벨편차", "시작거리", "타워명", "타워번호",
+          "속성유형", "현재날짜", "현재시간", "UTC날짜", "UTC시간", "위성연산", "위성추적중", "수평오차", "수직오차",
+          "Σn", "σe", "PDOP", "HDOP", "VDOP", "저장모드", "솔루션 정보", "Detailed Solution Status", "지연",
+          "컷 오프 각도", "반복횟수", "안테나 높이", "속도", "방향 기준국 ID", "기준국 위도", "기준국 경도",
+          "기준국 고도", "기준국과 거리", "원점 위도", "원점 경도", "원점 고도", "Station Correction x",
+          "Station Correction y", "Station Correction h", "경사 보정", "Pitch", "Roll", "Yaw", "경사 각도", "투영 각도",
+          "X보정", "Y보정", "Z보정", "공분산 Cxx", "공분산 Cxy", "공분산 Cxz", "공분산 Cyx", "공분산 Cyy", "공분산 Cyz",
+          "공분산 Czx", "공분산 Czy", "공분산 Czz", "SD", "HD", "VD", "HA", "VA", "PPM", "참조각도", "알려진 방위각",
+          "스테이션 좌표 북쪽", "스테이션 좌표 동쪽", "스테이션 좌표 높이", "반사경", "HI", "프리즘상수", "HT")
+
+
   }
 }
